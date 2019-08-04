b958291912f1df80262e5744ff4f834654fa1a5b
Salikov.Dmitriy <salikov.dmitriy89@mail.ru>
2019-08-04 21:28:18 +0300

    Update FileDBClient.

--- AngularApp/FileDBClient/src/app/app.module.ts
+++ AngularApp/FileDBClient/src/app/app.module.ts
@@ -4,16 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {HomeComponent} from './home/home.component';
-import {NavMenuComponent} from './nav-menu/nav-menu.component';
-import {FooterComponent} from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { TitleComponent } from './home/title-component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavMenuComponent,
-    FooterComponent
+    FooterComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
